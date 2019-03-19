e01592aeecc95145d036a5260fb6d53296adc3dc
Aulia Rahman <auliar68@gmail.com>
2019-03-19 15:36:57 +0700

    berhasil ngedetect cuy

--- app/src/main/java/id/co/coffeecode/learntextrecognitionapps/MainActivity.java
+++ app/src/main/java/id/co/coffeecode/learntextrecognitionapps/MainActivity.java
@@ -8,14 +8,24 @@ import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.util.List;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -93,7 +103,49 @@ public class MainActivity extends AppCompatActivity {
 
                 BitmapDrawable bitmapDrawable = (BitmapDrawable) ivPreview.getDrawable();
                 Bitmap bitmap = bitmapDrawable.getBitmap();
+
+                processTextRecognition(bitmap);
             }
+        }
+    }
+
+    private void processTextRecognition(Bitmap bitmap) {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVisionTextRecognizer recognizer = FirebaseVision.getInstance()
+                .getOnDeviceTextRecognizer();
+        recognizer.processImage(image)
+                .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                    @Override
+                    public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                        Log.d("coba", "hasil : "+firebaseVisionText.getText());
+                        String result = firebaseVisionText.getText();
+                        tvResult.setText(result);
+
+                        processIdCard(firebaseVisionText);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+    }
+
+    private void processIdCard(FirebaseVisionText firebaseVisionText) {
+        List<FirebaseVisionText.TextBlock> blocks = firebaseVisionText.getTextBlocks();
+        StringBuilder result = new StringBuilder();
+        for (FirebaseVisionText.TextBlock block : blocks){
+            result.append(block.getText());
+        }
+
+        String finalResult = result.toString().trim();
+
+        Log.d("coba", "hasil from procesID : "+result.toString());
+        if (finalResult.equals("AULIA RAHMAN")){
+           Log.d("coba", "berhasil");
+        }else {
+            Log.d("coba", "gagal");
         }
     }
 }
