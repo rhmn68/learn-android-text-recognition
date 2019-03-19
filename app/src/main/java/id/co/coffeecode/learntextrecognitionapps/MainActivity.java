794741132e0726dbd8a6643fead18e5adc870f7a
Aulia Rahman <auliar68@gmail.com>
2019-03-19 13:23:14 +0700

    belum jadi apa2

--- app/src/main/java/id/co/coffeecode/learntextrecognitionapps/MainActivity.java
+++ app/src/main/java/id/co/coffeecode/learntextrecognitionapps/MainActivity.java
@@ -1,13 +1,78 @@
 package id.co.coffeecode.learntextrecognitionapps;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+
+import com.camerakit.CameraKitView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CameraKitView cameraKitView;
+    private Button btnCaputre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cameraKitView = findViewById(R.id.camera);
+        btnCaputre = findViewById(R.id.btnCapture);
+
+        btnCaputre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraKitView.captureImage(new CameraKitView.ImageCallback() {
+                    @Override
+                    public void onImage(CameraKitView cameraKitView, byte[] bytes) {
+                        File savedPhoto = new File(Environment.getExternalStorageDirectory(), "photo.jpg");
+                        try {
+                            FileOutputStream outputStream = new FileOutputStream(savedPhoto.getPath());
+                            outputStream.write(bytes);
+                            outputStream.close();
+                        }catch (java.io.IOException e){
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        cameraKitView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraKitView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        cameraKitView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        cameraKitView.onStop();
+        super.onStop();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
