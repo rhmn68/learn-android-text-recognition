6df04f7661134ba801d2add1612d0393a7273cff
Aulia Rahman <auliar68@gmail.com>
2019-03-19 14:08:46 +0700

    add camera

--- app/src/main/java/id/co/coffeecode/learntextrecognitionapps/MainActivity.java
+++ app/src/main/java/id/co/coffeecode/learntextrecognitionapps/MainActivity.java
@@ -1,78 +1,99 @@
 package id.co.coffeecode.learntextrecognitionapps;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.camerakit.CameraKitView;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
-import java.io.File;
-import java.io.FileOutputStream;
-
-import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CameraKitView cameraKitView;
-    private Button btnCaputre;
+    Button btnCaptureImage;
+    ImageView ivPreview;
+    TextView tvResult;
+
+    String cameraPermission[];
+
+    private static final int IMAGE_PICK_CAMErA_CODE = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cameraKitView = findViewById(R.id.camera);
-        btnCaputre = findViewById(R.id.btnCapture);
+        btnCaptureImage = findViewById(R.id.btnCaptureImage);
+        ivPreview = findViewById(R.id.ivPreview);
+        tvResult = findViewById(R.id.tvResult);
 
-        btnCaputre.setOnClickListener(new View.OnClickListener() {
+        btnCaptureImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cameraKitView.captureImage(new CameraKitView.ImageCallback() {
-                    @Override
-                    public void onImage(CameraKitView cameraKitView, byte[] bytes) {
-                        File savedPhoto = new File(Environment.getExternalStorageDirectory(), "photo.jpg");
-                        try {
-                            FileOutputStream outputStream = new FileOutputStream(savedPhoto.getPath());
-                            outputStream.write(bytes);
-                            outputStream.close();
-                        }catch (java.io.IOException e){
-                            e.printStackTrace();
-                        }
-                    }
-                });
+                showCamera();
             }
         });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        cameraKitView.onStart();
+    private void showCamera() {
+        if (!checkCameraPermission()){
+            requestCameraPermission();
+        }else {
+            pickCamera();
+        }
+    }
+
+    private void pickCamera() {
+        Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intentCamera, IMAGE_PICK_CAMErA_CODE);
+    }
+
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(this, cameraPermission, 200);
+    }
+
+    private boolean checkCameraPermission() {
+        boolean resultCamera = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) == (PackageManager.PERMISSION_GRANTED);
+
+        return resultCamera;
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        cameraKitView.onResume();
-    }
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            if (requestCode == IMAGE_PICK_CAMErA_CODE){
+                CropImage.activity(data.getData())
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(this);
+            }
+        }
 
-    @Override
-    protected void onPause() {
-        cameraKitView.onPause();
-        super.onPause();
-    }
+        //get croped image
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK){
+                Uri resultUri = result.getUri();
+                //set image to preview
+                ivPreview.setImageURI(resultUri);
 
-    @Override
-    protected void onStop() {
-        cameraKitView.onStop();
-        super.onStop();
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) ivPreview.getDrawable();
+                Bitmap bitmap = bitmapDrawable.getBitmap();
+            }
+        }
     }
 }
